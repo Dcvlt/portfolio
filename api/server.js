bae08427e70112d62d3772b73763c93bc24d59f0
Pierre Imhoff <p.imhoff@redpsher.com>
2023-07-04 11:16:13 +0200

    implement vite client & cleanup docker

--- api/server.js
+++ api/server.js
@@ -9,19 +9,27 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+app.use(
+    express.static(path.join(__dirname, 'dist'), {
+        setHeaders: (res, path) => {
+            if (path.endsWith('.js')) {
+                res.setHeader('Content-Type', 'application/javascript');
+            }
+        }
+    })
+);
+
 app.get('/', (req, res) => {
-  res.send({ express: 'Yup' });
+    res.send({ express: 'Yup' });
 });
 
 app.get('/api/getlist', (req, res) => {
     res.send({ express: 'Hello From Express' });
-  });
-  
+});
+
 app.post('/api/postlist', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+    console.log(req.body);
+    res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
 });
 
 app.listen(PORT, HOST);
